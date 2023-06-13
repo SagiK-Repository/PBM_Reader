538d12d5767ccd751b27730964820076f230795c
박주형 <juhyung.park@mirero.co.kr>
2023-06-13 18:02:40 +0900

    Add MainView.xaml, MainViewModel.cs

---
+++ PBM_Reader/View/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Xpf.Core;
+using PBM_Reader.ViewModel;
+
+namespace PBM_Reader.View
+{
+    /// <summary>
+    /// MainView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : DXWindow
+    {
+        public MainView(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ PBM_Reader/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBM_Reader.ViewModel
+{
+    public class MainViewModel
+    {
+        #region Constructor
+        public MainViewModel() { }
+        #endregion
+    }
+}
