22505fd68d679c34f40068d4f2fe53bc9ffe7616
SagiKPJH1021 <Juhyung1021@gmail.com>
2023-06-18 17:50:13 +0900

    Add Petabridge Test

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
-using PBM_Reader.Common.Static;
 
 namespace ConsoleTest
 {
@@ -12,6 +8,30 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter IP address:");
+            string ipAddress = Console.ReadLine();
+
+            Console.WriteLine("Enter port:");
+            int port = int.Parse(Console.ReadLine());
+
+            // Petabridge 서버에 연결
+            using (TcpClient client = new TcpClient(ipAddress, port))
+            {
+                NetworkStream stream = client.GetStream();
+
+                // "cluster show" 명령어 전송
+                byte[] commandBytes = Encoding.ASCII.GetBytes("cluster show\n");
+                stream.Write(commandBytes, 0, commandBytes.Length);
+
+                // 결과 수신
+                byte[] buffer = new byte[4096];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string result = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                // 결과 출력
+                Console.WriteLine("Cluster show result:");
+                Console.WriteLine(result);
+            }
 
         }
     }
