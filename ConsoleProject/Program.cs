7b66d296a37f72b505d91d0ac05c3f2eedc83508
박주형 <juhyung.park@mirero.co.kr>
2023-08-10 14:30:02 +0900

    불필요한 프로젝트 제거

--- ConsoleProject/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using PBM_Reader.Common.Static;
-
-string command = "Get-Process";
-PowerShellHandling powerShellHandling = new PowerShellHandling();
-
-List<string> outputLines = powerShellHandling.ExecuteCommandLines(command);
-
-for (int i = 0; i < outputLines.Count; i++)
-    Console.WriteLine($"[{i}] - {outputLines[i]}");
