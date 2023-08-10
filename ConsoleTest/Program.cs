a234561c7a9b62aca607cef168fc499a0c6290fe
박주형 <juhyung.park@mirero.co.kr>
2023-08-10 09:05:58 +0900

    Petabridge, Actor 자체 생산, 자체 접근, 자체 상태 확인 코드

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,29 +1,84 @@
-﻿using Akka.Actor;
-using Petabridge.Cmd.Common.Client;
-using Petabridge.Cmd.Remote;
-using System;
-using System.Threading.Tasks;
+﻿using System;
+using Akka.Actor;
+using Akka.Cluster;
 
-namespace MyAkkaApp
+class Program
 {
-    public static class Program
+    static void Main(string[] args)
     {
-        public static async Task Main()
+        var config = @"
+            akka {
+                actor {
+                    provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+                }
+                remote {
+                    log-remote-lifecycle-events = off
+                    dot-netty.tcp {
+                        hostname = ""127.0.0.1""
+                        port = 9100
+                    }
+                }
+                cluster {
+                    seed-nodes = [""akka.tcp://mls-cluster-system@127.0.0.1:9100""]
+                }
+            }";
+
+        var system = ActorSystem.Create("mls-cluster-system", config);
+        var cluster = Cluster.Get(system);
+
+        var clusterWatcher = system.ActorOf(Props.Create(() => new ClusterWatcher()));
+
+        while (true)
         {
-            var host = "127.0.0.1";
-            var port = 9100;
+            Console.WriteLine("Press ENTER to check the cluster status or type 'exit' to leave...");
+            var input = Console.ReadLine();
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
 
-            // Create a Petabridge.Cmd client
-            var client = await PetabridgeCmdClient.CreateAsync(host, port);
+            clusterWatcher.Tell("CheckStatus");
+        }
 
-            // Send "cluster show" command to the remote server
-            var response = await client.ExecCommandAsync("cluster show");
+        cluster.Leave(cluster.SelfAddress);
+        system.Terminate().Wait();
 
-            // Display response from the remote server
-            Console.WriteLine(response);
 
-            // Shutdown the client
-            await client.ShutdownAsync();
+        Console.ReadLine();
+    }
+}
+
+public class ClusterWatcher : ReceiveActor
+{
+    public ClusterWatcher()
+    {
+        var cluster = Cluster.Get(Context.System);
+
+        Receive<string>(statusCheck => statusCheck == "CheckStatus", statusCheck =>
+        {
+            Console.WriteLine("Requesting cluster status...");
+            cluster.SendCurrentClusterState(Self);
+        });
+
+        Receive<ClusterEvent.CurrentClusterState>(state =>
+        {
+            ShowClusterState(state);
+        });
+    }
+
+    private void ShowClusterState(ClusterEvent.CurrentClusterState state)
+    {
+        Console.WriteLine("Cluster state:");
+        Console.WriteLine("Members:");
+        foreach (var member in state.Members)
+        {
+            Console.WriteLine($"\t{member.Address}: {member.Status}");
+        }
+
+        Console.WriteLine("Roles:");
+        foreach (var role in state.AllRoles)
+        {
+            Console.WriteLine($"\t{role}");
         }
     }
 }
